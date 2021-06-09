9bf3923cb5b2849f7c07a59f9defdd3ffc0e0179
Jim Gerth <jim.frederic.gerth@gmail.com>
2021-06-09 21:20:00 +0200

    model/corner: Add Corner class with radius
    Combine the CornerType enum and a radius to a general Corner class.
    
    Signed-off-by: Jim Gerth <jim.frederic.gerth@gmail.com>

--- lib/src/model/corner.dart
+++ lib/src/model/corner.dart
@@ -26,3 +26,36 @@ enum CornerShape {
   // TODO: Add comment.
   inset,
 }
+
+/// A class representing corners of a two dimensional shape.
+///
+/// A [Corner] has a specific [CornerShape] and an optional
+/// [radius], which is only required for some corner shapes.
+class Corner {
+  /// Create a new [Corner] object.
+  ///
+  /// The [shape] defaults to [CornerShape.hard].
+  ///
+  /// The [radius] defaults to zero.
+  Corner({
+    CornerShape? shape,
+    num? radius,
+  })  : shape = shape ?? CornerShape.sharp,
+        radius = (radius ?? 0).toDouble().clamp(0, 1);
+
+  /// The shape of the corner.
+  ///
+  /// See [CornerShape] for more.
+  final CornerShape shape;
+
+  /// The radius of the corner.
+  ///
+  /// This is a decimal percentage factor setting
+  /// the corner radius to anywhere between the
+  /// minimum and maximum possible radius and is
+  /// thus clamped to be between 0 and 1.
+  ///
+  /// This has no effect with a [shape] of
+  /// [CornerShape.sharp] or [CornerShape.inset].
+  final double radius;
+}
